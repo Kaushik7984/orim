0dc289db22af3e8c168c0cf793b0c88ac2f08f7a
Kaushik7984 <kaushik7984@gmail.com>
2025-04-14 16:49:17 +0530

    add create board button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,13 @@
+import BoardCreateButton from "./_components/board-create-button";
 import BoardList from "./_components/board-list";
 
 export default function DashboardPage() {
   return (
-    <div>
+    <div className='space-y-6'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-2xl font-bold'>Dashboard</h1>
+        <BoardCreateButton />
+      </div>
       <BoardList />
     </div>
   );
