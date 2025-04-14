b687cce0f575563754a03d865ab50166fe107405
Kaushik7984 <kaushik7984@gmail.com>
2025-04-14 16:43:27 +0530

    Add board-list component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
+import BoardList from "./_components/board-list";
+
 export default function DashboardPage() {
   return (
     <div>
-      <h1 className='text-2xl font-bold'>Welcome to your Dashboard</h1>
-      <p className='text-gray-600 mt-2'>boards</p>
+      <BoardList />
     </div>
   );
 }
