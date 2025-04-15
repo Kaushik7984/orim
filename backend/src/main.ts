19516a13d75488ce25a1e7ec5e44e024a23ffa78
Kaushik7984 <kaushik7984@gmail.com>
2025-04-15 18:26:04 +0530

    add cookiparser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
+
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
 
