d9b8a6150256e6e970e16761c96e01df0baefed8
Kaushik7984 <kaushik7984@gmail.com>
2025-04-15 15:17:28 +0530

    update file path , coz currently use other code

--- src/app/dashboard/board/[boardId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import CanvasWrapper from "./Canvas/CanvasWrapper";
-
-export default function BoardPage() {
-  return (
-    <div className='relative w-full h-screen bg-gray-100'>
-      <CanvasWrapper />
-    </div>
-  );
-}
