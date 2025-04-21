92ac5e97c031858a41c524f73f2d84a97d65e3d9
Kaushik7984 <kaushik7984@gmail.com>
2025-04-21 15:57:34 +0530

    fix socket error

--- backend/src/boards/boards.module.ts
+++ backend/src/boards/boards.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
 import { Board, BoardSchema } from './schemas/board.schema';
+import { BoardGateway } from './board.gateway';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Board.name, schema: BoardSchema }]),
   ],
   controllers: [BoardsController],
-  providers: [BoardsService],
+  providers: [BoardsService, BoardGateway],
 })
 export class BoardsModule {}
